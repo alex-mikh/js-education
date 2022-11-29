a570b6cfdc55628436a270540410ccaaa8b62d69
Rdnaxel <polimiorph@gmail.com>
2022-11-29 12:05:19 +0700

    test

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict"; /* jshint -W097 */
+
+
+// const result = confirm("Da, net?");
+// console.log(result);
+
+// const answer = +prompt("Are you over 18 years old?", "18");
+// console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt('Name', "");
+answers[1] = prompt('2nd name', "");
+answers[2] = prompt('Age', "");
+
+document.write(typeof(answers));
+
