75f20b707ddb61125d1c9acd848cb27f1cbde7f4
Rdnaxel <polimiorph@gmail.com>
2022-11-30 19:32:44 +0700

    delete

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 "use strict"; /* jshint -W097 */
 
-const numberOfFilms = prompt("How many films have you watched?", "");
-console.log(numberOfFilms);
+const numberOfFilms = prompt("How maaaaany films have you watched?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
 
 
+const lastFilm = prompt("What was the last movie you watched?", "");
+const rate = prompt("What rating did you give him?", "1-10");
 
+
+personalMovieDB.movies[lastFilm] = rate;
+
+console.log(personalMovieDB);
